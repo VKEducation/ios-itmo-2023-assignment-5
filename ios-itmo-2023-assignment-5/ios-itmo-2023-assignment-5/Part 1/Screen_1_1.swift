abcf431f1300bc338ad8307513b2a917b6c1c6ea
Ertem Biyik <e.biyik@vk.team>
2023-11-27 11:33:54 +0300

    Add project

---
+++ ios-itmo-2023-assignment-5/ios-itmo-2023-assignment-5/Part 1/Screen_1_1.swift
@@ -0,0 +1,8 @@
+//
+//  Screen_1_1.swift
+//  ios-itmo-2023-assignment-5
+//
+//  Created by e.biyik on 27.11.2023.
+//
+
+import Foundation
---
+++ ios-itmo-2023-assignment-5/ios-itmo-2023-assignment-5/ios_itmo_2023_assignment_5App.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct ios_itmo_2023_assignment_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
